f291b17dd38a5bb9948672a4904891cd6d1a095a
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2020-12-09 21:13:53 +0100

    Add day8

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -3,9 +3,9 @@ extern crate lazy_static;
 extern crate regex;
 
 use regex::Regex;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
 use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let p = parse_rule_file(BufReader::new(File::open("input.txt").unwrap())).unwrap();
@@ -15,27 +15,30 @@ fn main() {
 
 lazy_static! {
     static ref HEAD_RE: Regex = Regex::new(r"^(\w+ \w+) bags $").unwrap();
-    static ref TAIL_RE: Regex = {
-        Regex::new(r"^(?P<num>\d+) (?P<kind>\w+ \w+) bags?\.?$").unwrap()
-    };
+    static ref TAIL_RE: Regex =
+        { Regex::new(r"^(?P<num>\d+) (?P<kind>\w+ \w+) bags?\.?$").unwrap() };
 }
 
-// Input is of kind: 
+// Input is of kind:
 // big red bags contain 1 dotted pink bag, 3 metallic orange bags.
 fn parse_rule(string: &str) -> Option<(String, Vec<(usize, String)>)> {
     let mut headtail = string.split("contain ");
 
     // Verify head (the thing before "contain ")
-    let match1 = HEAD_RE.captures(headtail.next()?)?.get(1)?.as_str().to_string();
+    let match1 = HEAD_RE
+        .captures(headtail.next()?)?
+        .get(1)?
+        .as_str()
+        .to_string();
     let tail = headtail.next()?;
     if headtail.next().is_some() {
-        return None
-    } 
+        return None;
+    }
 
     // Verify tail (the thing after "contain ")
     let mut vec: Vec<(usize, String)> = Vec::new();
     if tail == "no other bags." {
-        return Some((match1, vec))
+        return Some((match1, vec));
     }
 
     // Fill in tail
@@ -59,20 +62,19 @@ fn parse_rule_file<T: BufRead>(reader: T) -> Result<Vec<Bag>, ParserError> {
     for (lineno, lineres) in reader.lines().enumerate() {
         let line = match lineres {
             Ok(line) => line,
-            Err(_) => return Err(ParserError(lineno))
+            Err(_) => return Err(ParserError(lineno)),
         };
         if line.trim().is_empty() {
             continue;
         }
         let elem = match parse_rule(line.trim()) {
             Some(e) => e,
-            None => return Err(ParserError(lineno))
+            None => return Err(ParserError(lineno)),
         };
         result.push(elem);
     }
     Ok(result)
 }
-
 
 // Map from e.g. "dotted blue" => [(3, "wavy gold"), (1, "dark maroon")]
 fn inner_outer_hashmap(v: &[Bag]) -> HashMap<String, Vec<String>> {
@@ -80,8 +82,10 @@ fn inner_outer_hashmap(v: &[Bag]) -> HashMap<String, Vec<String>> {
     for (outer, inners) in v {
         for (_n, inner) in inners {
             match map.get_mut(inner) {
-                None => {map.insert(inner.clone(), vec![outer.clone()]);},
-                Some(v) => v.push(outer.clone())
+                None => {
+                    map.insert(inner.clone(), vec![outer.clone()]);
+                }
+                Some(v) => v.push(outer.clone()),
             }
         }
     }
@@ -92,19 +96,23 @@ fn inner_outer_hashmap(v: &[Bag]) -> HashMap<String, Vec<String>> {
 // seen, by keeping a set of processed types of bags
 fn part_1(inner: &str, bags: &[Bag]) -> usize {
     let map = inner_outer_hashmap(bags);
-    let mut unprocessed: Vec<String> = map.get(inner).expect("Input string not in map")
-    .iter().map(|s| s.to_owned()).collect();
+    let mut unprocessed: Vec<String> = map
+        .get(inner)
+        .expect("Input string not in map")
+        .iter()
+        .map(|s| s.to_owned())
+        .collect();
     let mut processed: HashSet<String> = HashSet::new();
     loop {
         let bag = match unprocessed.pop() {
             None => return processed.len(),
-            Some(b) => b
+            Some(b) => b,
         };
         processed.insert(bag.to_string());
         if let Some(outers) = map.get(&bag) {
             for outer in outers {
                 if processed.contains(outer) {
-                    continue
+                    continue;
                 };
                 unprocessed.push(outer.to_string());
             }
@@ -119,20 +127,28 @@ fn part_1(inner: &str, bags: &[Bag]) -> usize {
 // If we do one round without inferring at least one new bag, and without having already
 // inferred our target bag, the problem is unsolvable and we panic.
 fn part_2(outer: &str, bags: &[Bag]) -> usize {
-    let mut containing_bags: HashMap<String, usize> = bags.iter()
-    .filter(|(_h, v)| v.is_empty()).map(|(h, _v)| (h.clone(), 0)).collect();
+    let mut containing_bags: HashMap<String, usize> = bags
+        .iter()
+        .filter(|(_h, v)| v.is_empty())
+        .map(|(h, _v)| (h.clone(), 0))
+        .collect();
 
-    let mut remaining = bags.iter()
-    .filter(|(h, _v)| !containing_bags.contains_key(h))
-    .map(|(s,v)| (s, v)).collect::<Vec<_>>();
+    let mut remaining = bags
+        .iter()
+        .filter(|(h, _v)| !containing_bags.contains_key(h))
+        .map(|(s, v)| (s, v))
+        .collect::<Vec<_>>();
     let mut skipped = Vec::new();
-    
+
     while !containing_bags.contains_key(outer) {
         let n_elem = remaining.len();
-        
+
         for (outer, v) in remaining.drain(..) {
             if v.iter().all(|(_n, h)| containing_bags.contains_key(h)) {
-                let n = v.iter().map(|(n, h)| n * containing_bags.get(h).unwrap() + n).sum();
+                let n = v
+                    .iter()
+                    .map(|(n, h)| n * containing_bags.get(h).unwrap() + n)
+                    .sum();
                 containing_bags.insert(outer.to_string(), n);
             } else {
                 skipped.push((outer, v));
@@ -143,5 +159,5 @@ fn part_2(outer: &str, bags: &[Bag]) -> usize {
         };
         std::mem::swap(&mut remaining, &mut skipped);
     }
-    *containing_bags.get(outer).unwrap()    
+    *containing_bags.get(outer).unwrap()
 }
---
+++ day8/src/main.rs
@@ -0,0 +1,66 @@
+fn main() {
+    let mut instructions = parse_instructions("input.txt");
+    println!("{}", part1(&instructions).1);
+    println!("{}", part2(&mut instructions));
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Instruction {
+    Nop,
+    Jmp,
+    Acc
+}
+#[derive(Debug, Clone, Copy)]
+struct Command(Instruction, i32);
+
+fn parse_instructions(path: &str) -> Vec<Command> {
+    std::fs::read_to_string(path).unwrap().trim().lines()
+    .map(|line| line.split(' ')).map(|mut fields| {
+        let instruction = match fields.next().unwrap() {
+            "nop" => Instruction::Nop,
+            "jmp" => Instruction::Jmp,
+            "acc" => Instruction::Acc,
+            _ => panic!()
+        };
+        Command(instruction, fields.next().unwrap().parse::<i32>().unwrap())
+    }).collect()
+}
+
+// Returns completed_successfully, acc
+fn part1(instructions: &[Command]) -> (bool, i32) {
+    let mut visited = vec![false; instructions.len()];
+    let mut pointer: usize = 0;
+    let mut acc = 0;
+    loop {
+        if pointer == visited.len() {
+            return (true, acc)
+        }
+        if visited[pointer] {
+            return (false, acc)
+        }
+        visited[pointer] = true;
+        match instructions[pointer] {
+            Command(Instruction::Nop, _) => pointer += 1,
+            Command(Instruction::Jmp, x) => pointer = ((pointer as i32) + x) as usize,
+            Command(Instruction::Acc, x) => {acc += x as i32; pointer += 1}
+        }        
+    }
+}
+
+fn part2(instructions: &mut [Command]) -> i32 {
+    for i in 0..instructions.len() {
+        let oldinstr = instructions[i];
+        let newinstr = match oldinstr  {
+            Command(Instruction::Acc, _) => continue,
+            Command(Instruction::Nop, x) => Command(Instruction::Jmp, x),
+            Command(Instruction::Jmp, x) => Command(Instruction::Nop, x)
+        };
+        instructions[i] = newinstr;
+        let (terminated, acc) = part1(instructions);
+        instructions[i] = oldinstr;
+        if terminated {
+            return acc
+        }
+    }
+    panic!("No changes could save the program");
+}
